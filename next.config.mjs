14d82d7fb7d0a1f5383248899cf6576a0ccffbb0
MaheshtheDev <mahesh.svmr@gmail.com>
2024-07-31 19:48:37 -0700

    feat: Add PWA Support

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,25 @@
-import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
+import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
+import pwa from "next-pwa";
 
 // Here we use the @cloudflare/next-on-pages next-dev module to allow us to use bindings during local development
 // (when running the application with `next dev`), for more information see:
 // https://github.com/cloudflare/next-on-pages/blob/main/internal-packages/next-dev/README.md
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === "development") {
   await setupDevPlatform();
 }
+
+const withPWA = pwa({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  register: true,
+  skipWaiting: true,
+});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    ignoreDuringBuilds: true
-  }
+    ignoreDuringBuilds: true,
+  },
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
